def8f94a132f0684870f802a6e46b34b941cc101
Akshitpatel25 <patelakshit1225@gmail.com>
2024-06-02 17:39:01 +0530

    newlocation

--- script.js
+++ script.js
@@ -1,76 +1,98 @@
-fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_lF7JrQw14fP3lWUMrdUyE63NQWR06&ipAddress=")
-.then(response => {
+fetch(
+  "https://geo.ipify.org/api/v2/country,city?apiKey=at_lF7JrQw14fP3lWUMrdUyE63NQWR06&ipAddress="
+)
+  .then((response) => {
     if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
+      throw new Error("Network response was not ok " + response.statusText);
     }
-    return response.json(); 
-})
-.then(data => {
+    return response.json();
+  })
+  .then((data) => {
     // console.log(data);
     let lat = data.location["lat"];
     let lng = data.location["lng"];
-    
+
     // Create an object to store both lat and lng
     let locationData = {
-        lat: lat,
-        lng: lng
+      lat: lat,
+      lng: lng,
     };
     // Convert the object to a JSON string and store it in local storage
-    localStorage.setItem('location', JSON.stringify(locationData));
-    
+    localStorage.setItem("location", JSON.stringify(locationData));
 
     document.querySelector(".ip_output_part").innerHTML = data.ip;
-    document.querySelector(".location_output_part").innerHTML = `${data.location["country"]}, ${data.location["city"]} <br/> ${data.location["postalCode"]}`;
-    document.querySelector(".timezone_output_part").innerHTML = `${data.location["timezone"]}`;
+    document.querySelector(
+      ".location_output_part"
+    ).innerHTML = `${data.location["country"]}, ${data.location["city"]} <br/> ${data.location["postalCode"]}`;
+    document.querySelector(
+      ".timezone_output_part"
+    ).innerHTML = `${data.location["timezone"]}`;
     document.querySelector(".ISP_output_part").innerHTML = `${data.as["name"]}`;
-})
-.catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-});
+  })
+  .catch((error) => {
+    console.error("There has been a problem with your fetch operation:", error);
+  });
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++
 
-
 document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const input = document.querySelector("#ip_input").value;
+  const input = document.querySelector("#ip_input").value;
 
-    checkIPAddress(input);
-})
+  checkIPAddress(input);
+});
 
 function checkIPAddress(ip) {
-    const ipv4Pattern = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
+  const ipv4Pattern = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
 
-    if (ipv4Pattern.test(ip)) {
-        console.log("Valid IPv4 address");
-        document.querySelector("#ip_input").style.borderColor = "white";
+  if (ipv4Pattern.test(ip)) {
+    console.log("Valid IPv4 address");
+    document.querySelector("#ip_input").style.borderColor = "white";
 
-        // let ipaddress = "https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_kP07TZHPpvl1Yi0xj8GbD0eyEKn8X&ipAddress=";
+    let ipaddress = "https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_kP07TZHPpvl1Yi0xj8GbD0eyEKn8X&ipAddress=";
 
-        ipaddress += ip;
+    ipaddress += ip;
 
-        fetch(ipaddress)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            // console.log(data);
-            
-            document.querySelector(".ip_output_part").innerHTML = data.ip;
-            document.querySelector(".location_output_part").innerHTML = `${data.location["country"]}, ${data.location["city"]} <br/> ${data.location["postalCode"]}`;
-            document.querySelector(".timezone_output_part").innerHTML = `${data.location["timezone"]}`;
-            document.querySelector(".ISP_output_part").innerHTML = `${data.as["name"]}`;
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
+    fetch(ipaddress)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        let lat = data.location["lat"];
+        let lng = data.location["lng"];
 
-    } else {
-        alert("wrong IP Address")
-        document.querySelector("#ip_input").style.borderColor = "red";
-    }
+        // Create an object to store both lat and lng
+        let newlocationData = {
+          newlat: lat,
+          newlng: lng,
+        };
+        // Convert the object to a JSON string and store it in local storage
+        localStorage.setItem("newlocation", JSON.stringify(newlocationData));
+
+        document.querySelector(".ip_output_part").innerHTML = data.ip;
+        document.querySelector(
+          ".location_output_part"
+        ).innerHTML = `${data.location["country"]}, ${data.location["city"]} <br/> ${data.location["postalCode"]}`;
+        document.querySelector(
+          ".timezone_output_part"
+        ).innerHTML = `${data.location["timezone"]}`;
+        document.querySelector(
+          ".ISP_output_part"
+        ).innerHTML = `${data.as["name"]}`;
+      })
+      .catch((error) => {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      });
+  } else {
+    alert("wrong IP Address");
+    document.querySelector("#ip_input").style.borderColor = "red";
+  }
 }
